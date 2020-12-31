5dc3f99bb1acd0fb42f6fccde06d69d6bd2ca7e1
rafaelguimaraes3 <rafael.guimaraes.tavares@gmail.com>
2020-12-31 11:01:12 -0300

    2

--- 1015.js
+++ 1015.js
@@ -2,5 +2,10 @@ var input = require("fs").readFileSync("stdin", "utf-8")
 
 var valores = input.split("\n")
 
-var [x1, x2] = input.split(" ")
-console.log(x2)
+var [x1, y1] = valores[0].split(" ")
+var [x2, y2] = valores[1].split(" ")
+
+var distancia = Math.sqrt(((x2 - x1) ** 2 + (y2 - y1) ** 2))
+
+
+console.log(distancia.toFixed(4))
