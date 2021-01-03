116b4f05d6ef976509f074377bfdbfe863054873
rafaelguimaraes3 <rafael.guimaraes.tavares@gmail.com>
2021-01-03 11:39:38 -0300

    commit

---
+++ 1018.js
@@ -0,0 +1,13 @@
+var input = require("fs").readFileSync("stdin", "utf-8")
+
+var valor = parseInt(input)
+
+var notas = [100, 50, 20, 10, 5, 2, 1]
+
+console.log(valor)
+
+for (var nota of notas) {
+    var qtNotas = parseInt(valor / nota)
+    console.log(`${qtNotas} nota(s) de R$ ${nota},00`)
+    valor %= nota
+}
---
+++ 1019.js
@@ -0,0 +1,7 @@
+var input = require("fs").readFileSync("stdin", "utf-8")
+
+var hora = parseInt(input) / 3600
+
+var minuto = parseInt(input) /
+
+    console.log(hora.toFixed(0))
