c85b9c76813d2f3e38ca2031543f19f5268a2ebf
rafaelguimaraes3 <rafael.guimaraes.tavares@gmail.com>
2021-01-04 22:31:58 -0300

    Commit

--- 1019.js
+++ 1019.js
@@ -1,7 +1,13 @@
 var input = require("fs").readFileSync("stdin", "utf-8")
 
-var hora = parseInt(input) / 3600
+var tempo = parseInt(input)
 
-var minuto = parseInt(input) /
+var horas = 3600
+var minutos = 60
+var segundos = 60
 
-    console.log(hora.toFixed(0))
+var converterHoras = parseInt(tempo / horas)
+var converterMinutos = parseInt((tempo % horas) / minutos)
+var converterSegundos = tempo % minutos
+
+console.log(`${converterHoras}:${converterMinutos}:${converterSegundos}`)
